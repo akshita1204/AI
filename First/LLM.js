ec7c7791e5f35213d3945c5392ebae35c0ce307e
akshita1204 <akshitatyagi04@gmail.com>
2025-06-23 07:34:18 +0530

    API prevented

--- First/LLM.js
+++ First/LLM.js
@@ -1,29 +1,50 @@
-
+import dotenv from "dotenv";
+dotenv.config();
 import { GoogleGenAI } from "@google/genai";
+import readLineSync from "readline-sync"
 
-const ai = new GoogleGenAI({ apiKey: "AIzaSyBGiGmUR-5uSVzmGYbhSrUi7NAdKQdpgtQ" });
+const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY });
+const History=[]
 
-async function main() {
+async function chatting(userProblem) {
+
+ History.push({
+        role:'user',
+        parts:[{text:userProblem}]
+    })
+
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
-    // contents: "What's my name?",
-    contents:[
-        {
-            role:"user",
-            parts:[{text:"Hi, I am Akshita"}]
-        },
-        {
-            role:"model",
-            parts:[{text:"Hi Akshita! It's nice to meet you.How can I help you today?"}]
-        },
-        {
-            role:"user",
-            parts:[{text:"What's my Name?"}]
-        },
+    contents: History,
+    // contents:[
+    //     {
+    //         role:"user",
+    //         parts:[{text:"Hi, I am Akshita"}]
+    //     },
+    //     {
+    //         role:"model",
+    //         parts:[{text:"Hi Akshita! It's nice to meet you.How can I help you today?"}]
+    //     },
+    //     {
+    //         role:"user",
+    //         parts:[{text:"What's my Name?"}]
+    //     },
        
-    ]
+    //]
+    
   });
+   History.push({
+        role:'model',
+        parts:[{text:response.text}]
+    })
   console.log(response.text);
 }
 
+async function main()
+{
+const userProblem=readLineSync.question("Ask me Anything----------->")
+await chatting(userProblem)
+main()
+
+}
 await main();
